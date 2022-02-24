6669f8f58c754e88716dd3414f27a1c476cf05e1
ivan-k2310 <ivankeller6@gmail.com>
2022-02-24 08:47:31 +0100

    background

--- index.php
+++ index.php
@@ -8,10 +8,35 @@
     <title>sushi place</title>
 </head>
 <body>
-    <main>
+    
+    <main> <!-- background image -->
         <nav class="navbar">
+            <div class="logoOrName">
+                <li><a href="#.php">link</a></li>
+            </div>
+            <div class="navSpaceBetween">
 
+            </div>
+            <div class="navLinkBox">
+                <div class="navBoxes">
+                    <li><a href="#.php">menu</a></li>
+                </div>
+                <div class="navBoxes">
+                    <li><a href="#.php">contact</a></li>
+                </div>
+                <div class="navBoxes">
+                    <li><a href="#.php">login</a></li>
+                </div>
+            </div>
         </nav>
+        <div class="backgroundContainer">
+            <div class="welcomeTextBox">
+                <a class="">welcome too sushi place</a>
+            </div>
+        </div>
     </main>
+    <div class="container">
+
+    </div>
 </body>
 </html>
