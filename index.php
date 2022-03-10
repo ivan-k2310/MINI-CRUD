d2333c550703381290f23c1a8260350b4b359ebc
ivan-k2310 <ivankeller6@gmail.com>
2022-03-10 08:52:01 +0100

    opacity 0

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <title>sushi place</title>
 </head>
+
 <body>
-    
-    <main> <!-- background image -->
+
+    <main>
+        <!-- background image -->
         <nav class="navbar">
             <div class="logoOrName">
                 <li><a href="#.php">link</a></li>
@@ -30,8 +35,23 @@
             </div>
         </nav>
         <div class="backgroundContainer">
-            <div class="welcomeTextBox">
-                <a class="">welcome too sushi place</a>
+            <div class="contentBox">
+                <div class="contenBoxTitle">
+                    <a class="title">sushi place</a>
+                </div>
+                <div class="contentBoxTextBox">
+                    <p class="text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Praesent aliquet urna nec dignissim varius.
+                        Nulla commodo enim sit amet mauris porta ultricies.
+                        Praesent nec velit eros. Sed pharetra facilisis arcu, in tempus ipsum vulputate non. <br>
+                        Praesent maximus, nisi a maximus efficitur, augue lorem tristique metus, in fringilla ligula
+                        enim a erat.
+                        Morbi consectetur vulputate ante. Nullam efficitur dui ac nunc hendrerit, ut venenatis mi porta.
+                        In quis posuere lacus. Donec sed dignissim est, eu euismod leo.</p>
+                    <br>
+
+                </div>
             </div>
         </div>
     </main>
@@ -39,4 +59,5 @@
 
     </div>
 </body>
+
 </html>
